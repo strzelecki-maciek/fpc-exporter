1b82bd27942dba498a8c99288483503ae866a577
Maciej <strzelecki.maciek@gmail.com>
2017-11-24 18:23:39 +0100

    savepoint

--- main.go
+++ main.go
@@ -11,53 +11,100 @@ import (
 	"mvdan.cc/xurls"
 	_ "reflect"
   "strings"
-	_ "time"
+	"time"
+  "sync"
   "github.com/prometheus/client_golang/prometheus"
   "github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	cpuTemp = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "cpu_temperature_celsius",
-		Help: "Current temperature of the CPU.",
+	fpc_load_time = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "fpc_load_time",
+		Help: "Time to load the landing page in seconds.",
 	})
-	hdFailures = prometheus.NewCounterVec(
+	fpc_load_failures = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "hd_errors_total",
-			Help: "Number of hard-disk errors.",
+			Name: "fpc_load_failures",
+			Help: "Number of times we failed to load a page.",
 		},
 		[]string{"device"},
 	)
 )
 
+func init() {
+	prometheus.MustRegister(cpuTemp)
+	prometheus.MustRegister(hdFailures)
+}
+
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
-func getContents(url string) string {
+func getContents(url string) (string, err) {
     var contents []byte
     fmt.Printf("getting contents for: %s\n", url)
     response, err := http.Get(url)
 		if err != nil {
 				fmt.Printf("%s", err)
-				os.Exit(1)
 		} else {
 				defer response.Body.Close()
 				contents, err = ioutil.ReadAll(response.Body)
 				if err != nil {
 						fmt.Printf("%s", err)
-						os.Exit(1)
 				}
 				//fmt.Printf("%s\n", string(contents))
 		}
-		return string(contents)
+		return string(contents), err
 }
 
 func checkUrl(url string) {
-  fmt.Printf("-> doing %s\n", url)
+  //fmt.Printf("-> doing %s\n", url)
 }
 
-func init() {
-	prometheus.MustRegister(cpuTemp)
-	prometheus.MustRegister(hdFailures)
+func doStuff(url string) {
+  time_start = time.Now()
+  c, err := getContents(url)
+  if err != nil {
+
+  }
+  else {
+
+  }
+  childUrls := xurls.Strict().FindAllString(c, -1)
+  //fmt.Printf("%v", childUrls)
+  for _, childUrl := range childUrls {
+    if strings.Contains(childUrl, "http://www.w3.org") {
+      fmt.Printf("X ignoring %s\n", childUrl)
+    } else {
+      checkUrl(childUrl)
+    }
+  }
+}
+
+func startChecking(urls []string) {
+  for {
+    const workers = 25
+
+    wg := new(sync.WaitGroup)
+    in := make(chan string, 2*workers)
+
+    for i:= 0; i < workers; i++ {
+      wg.Add(1)
+      go func() {
+        defer wg.Done()
+        for url := range in {
+          doStuff(url)
+        }
+      }()
+    }
+
+    for _, url := range urls {
+      in <- url
+    }
+    close(in)
+    wg.Wait()
+
+    fmt.Println("Sleeping...")
+    time.Sleep(5 * time.Second)
+  }
 }
 
 func main() {
@@ -66,17 +113,7 @@ func main() {
   urls := []string{}
   urls = append(urls, "http://www.wp.pl")
 
-  for _, url := range urls {
-	  childUrls := xurls.Strict().FindAllString(getContents(url), -1)
-		fmt.Printf("%v", childUrls)
-		for _, childUrl := range childUrls {
-			if strings.Contains(childUrl, "http://www.w3.org") {
-				fmt.Printf("X ignoring %s\n", childUrl)
-			} else {
-			  checkUrl(childUrl)
-			}
-    }
-  }
+  go startChecking(urls)
 
   cpuTemp.Set(65.3)
 	hdFailures.With(prometheus.Labels{"device":"/dev/sda"}).Inc()
