1e5378a876b85dfe0b0aad914f31234ea7d4fcba
Maciej <strzelecki.maciek@gmail.com>
2017-11-24 19:06:41 +0100

    savepoint

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
   "log"
 	"flag"
-	"os"
+  _ "os"
 	"io/ioutil"
 //	"log"
 	"net/http"
   "fmt"
 	"mvdan.cc/xurls"
 	_ "reflect"
+  "strconv"
   "strings"
 	"time"
   "sync"
@@ -18,41 +19,46 @@ import (
 )
 
 var (
-	fpc_load_time = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "fpc_load_time",
-		Help: "Time to load the landing page in seconds.",
-	})
-	fpc_load_failures = prometheus.NewCounterVec(
+	fpcLoadTime = prometheus.NewGaugeVec(
+    prometheus.GaugeOpts{
+      Name: "fpc_load_time",
+      Help: "Time to load the landing page in seconds.",
+    },
+    []string{"page", "statusCode"},
+  )
+  fpcLoadFailure = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "fpc_load_failures",
 			Help: "Number of times we failed to load a page.",
 		},
-		[]string{"device"},
+		[]string{"page", "statusCode"},
 	)
 )
 
 func init() {
-	prometheus.MustRegister(cpuTemp)
-	prometheus.MustRegister(hdFailures)
+	prometheus.MustRegister(fpcLoadTime)
+	prometheus.MustRegister(fpcLoadFailure)
 }
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
 
-func getContents(url string) (string, err) {
+func getContents(url string) (string, string, error) {
     var contents []byte
+    statusCode := "0"
     fmt.Printf("getting contents for: %s\n", url)
     response, err := http.Get(url)
 		if err != nil {
 				fmt.Printf("%s", err)
 		} else {
 				defer response.Body.Close()
+        statusCode = strconv.Itoa(response.StatusCode)
 				contents, err = ioutil.ReadAll(response.Body)
 				if err != nil {
 						fmt.Printf("%s", err)
 				}
 				//fmt.Printf("%s\n", string(contents))
 		}
-		return string(contents), err
+		return string(contents), statusCode, err
 }
 
 func checkUrl(url string) {
@@ -60,20 +66,18 @@ func checkUrl(url string) {
 }
 
 func doStuff(url string) {
-  time_start = time.Now()
-  c, err := getContents(url)
-  if err != nil {
+  time_start := time.Now()
+  c, statusCode, err := getContents(url)
 
+  if statusCode != "200" || err != nil {
+    fpcLoadFailure.With(prometheus.Labels{"page": url, "statusCode": statusCode}).Inc()
   }
-  else {
+  fpcLoadTime.With(prometheus.Labels{"page": url, "statusCode": statusCode}).Set(time.Since(time_start).Seconds())
 
-  }
   childUrls := xurls.Strict().FindAllString(c, -1)
   //fmt.Printf("%v", childUrls)
   for _, childUrl := range childUrls {
-    if strings.Contains(childUrl, "http://www.w3.org") {
-      fmt.Printf("X ignoring %s\n", childUrl)
-    } else {
+    if !strings.Contains(childUrl, "http://www.w3.org") {
       checkUrl(childUrl)
     }
   }
@@ -112,11 +116,9 @@ func main() {
 
   urls := []string{}
   urls = append(urls, "http://www.wp.pl")
+  urls = append(urls, "http://www.google.pl")
 
   go startChecking(urls)
-
-  cpuTemp.Set(65.3)
-	hdFailures.With(prometheus.Labels{"device":"/dev/sda"}).Inc()
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
